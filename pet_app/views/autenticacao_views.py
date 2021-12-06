a3c54c14945faf2d145c3cb4dfb238d7e70379f9
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2021-12-06 20:30:58 -0300

    returning commit

---
+++ pet_app/views/autenticacao_views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+def login_usuario(request):
+    if request.method == "POST":
+        form_login = AuthenticationForm(data=request.POST)
+        if form_login.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            usuario = authenticate(request, username=username, password=password)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('listar_clientes')
+            else:
+                messages.error(request, "As credenciais inseridas estão incorretas")
+                return redirect('login')
+    else:
+        form_login = AuthenticationForm()
+    return render(request, 'autenticacao/login.html', {'form_login': form_login})
+
+
+@login_required()
+def deslogar_usuario(request):
+    logout(request)
+    return redirect('login')
---
+++ pet_app/views/funcionario_views.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.hashers import make_password
+from django.shortcuts import render, redirect
+from ..forms.funcionario_forms import FuncionarioForm
+from ..entidades import funcionario
+from ..services import funcionario_service
+
+
+@user_passes_test(lambda u: u.cargo == 2)
+def listar_funcionarios(request):
+    funcionarios = funcionario_service.listar_funcionarios()
+    return render(request, 'funcionarios/lista_funcionarios.html', {'funcionarios': funcionarios})
+
+
+@user_passes_test(lambda u: u.cargo == 2)
+def inserir_funcionario(request):
+    if request.method == "POST":
+        form_funcionario = FuncionarioForm(request.POST)
+        if form_funcionario.is_valid():
+            nome = form_funcionario.cleaned_data["nome"]
+            nascimento = form_funcionario.cleaned_data["nascimento"]
+            cargo = form_funcionario.cleaned_data["cargo"]
+            username = form_funcionario.cleaned_data["username"]
+            password = make_password(form_funcionario.cleaned_data["password1"]) #pega o texto puro do conteudo da
+            #password e criptografa e transforma em um hash e salva no banco de dados
+            funcionario_novo = funcionario.Funcionario(nome=nome, 
+                                                    nascimento=nascimento, 
+                                                    cargo=cargo, 
+                                                    username=username, 
+                                                    password=password)
+            funcionario_service.cadastrar_funcionario(funcionario_novo)
+            return redirect('listar_funcionarios')
+    else:
+        form_funcionario = FuncionarioForm()
+    return render(request, 'funcionarios/form_funcionario.html', {'form_funcionario': form_funcionario})
