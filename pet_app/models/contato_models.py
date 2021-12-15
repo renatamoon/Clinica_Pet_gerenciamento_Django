db28c5ffea3324728197b1f53400ba51dced074e
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2021-12-15 15:50:54 -0300

    including contact models

--- pet_app/models/contato_models.py
+++ pet_app/models/contato_models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.core.validators import RegexValidator
+
 
 
 #models de Contato da Página Index - formulário para recepção de contatos
@@ -14,5 +16,12 @@ class TimeStampedModel(models.Model):
 class Cliente(TimeStampedModel):
     nome_completo            =       models.CharField    (   max_length=100, null=False, blank=False )
     email                    =       models.EmailField   (   null=False, blank=False )    
-    telefone                 =       models.CharField    (   max_length=14, unique=True, null=False, blank=True  )    
+    phoneNumberRegex         =       RegexValidator(regex = r"^\+?1?\d{8,15}$")
+    phoneNumber              =       models.CharField(validators = [phoneNumberRegex], max_length = 16, unique = True)   
     mensagem                 =       models.TextField    (   max_length=25, null= False, blank=False )
+
+
+
+#--------------obs:
+#O validador phoneNumberRegex valida o valor inserido para o CharField. 
+# Novamente, os números de telefone são armazenados no formato E.164.
