5116d5ed49972239ef9840c4f757a420db4fa018
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2021-12-20 19:10:09 -0300

    adjusting front end

---
+++ pet_app/static/js/myjs.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, options);
+});
+
+
+$(document).ready(function () {
+  $(".collapsible").collapsible();
+});
